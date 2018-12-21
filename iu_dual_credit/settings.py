6f7cd3191e414213f80bcc631777fe96d31c9c9f
smritisingh_mobiloitte <smriti.singh@mobiloitte.in>
2018-12-21 12:12:35 +0530

    MariaDB Setup

--- iu_dual_credit/settings.py
+++ iu_dual_credit/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'iu_dual_credit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dual_credit_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': "localhost",
-        'PORT': "5432"
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': "",
+        'PORT': ""
     }
 }
 
--- student_search/models.py
+++ student_search/models.py
@@ -1,143 +1,143 @@
-from django.db import models
+# from django.db import models
 
 
-class StudentDetails(models.Model):
+# class StudentDetails(models.Model):
 
-    # Demographic data
+#     # Demographic data
 
-    UID = models.CharField(
-        max_length=40, null=True, blank=True
-    )
-    recipient_email = models.EmailField(
-        max_length=40, unique=True
-    )
-    recipient_first_name = models.CharField(
-        max_length=40, null=True, blank=True
-    )
-    recipient_last_name = models.CharField(
-        max_length=40, null=True, blank=True
-    )
+#     UID = models.CharField(
+#         max_length=40, null=True, blank=True
+#     )
+#     recipient_email = models.EmailField(
+#         max_length=40, unique=True
+#     )
+#     recipient_first_name = models.CharField(
+#         max_length=40, null=True, blank=True
+#     )
+#     recipient_last_name = models.CharField(
+#         max_length=40, null=True, blank=True
+#     )
 
-    # Location
+#     # Location
 
-    latitude = models.DecimalField(
-        decimal_places=8, max_digits=12, blank=True, null=True
-    )
-    longitude = models.DecimalField(
-        decimal_places=8, max_digits=12, blank=True, null=True
-    )
+#     latitude = models.DecimalField(
+#         decimal_places=8, max_digits=12, blank=True, null=True
+#     )
+#     longitude = models.DecimalField(
+#         decimal_places=8, max_digits=12, blank=True, null=True
+#     )
 
-    # General
+#     # General
 
-    language = models.CharField(max_length=20, null=True, blank=True)
+#     language = models.CharField(max_length=20, null=True, blank=True)
 
-    legal_first_name = models.CharField(
-        max_length=40, null=True, blank=True
-    )
-    legal_last_name = models.CharField(
-        max_length=40, null=True, blank=True
-    )
-    educator_role = models.CharField(
-        "Great! We are excited to help you. To see what is available, please tell us a little more about your role as an educator. Select all that apply.", max_length=100, null=True, blank=True
-    )
-    institution = models.CharField(
-        "Which institution(s) are you affiliated with?", max_length=100, null=True, blank=True
-    )
-    purpose = models.CharField(
-        "What would you like to do?", max_length=100, null=True, blank=True)
-    start_session = models.CharField(
-        "When would you like to start?", max_length=100, null=True, blank=True
-    )
+#     legal_first_name = models.CharField(
+#         max_length=40, null=True, blank=True
+#     )
+#     legal_last_name = models.CharField(
+#         max_length=40, null=True, blank=True
+#     )
+#     educator_role = models.CharField(
+#         "Great! We are excited to help you. To see what is available, please tell us a little more about your role as an educator. Select all that apply.", max_length=100, null=True, blank=True
+#     )
+#     institution = models.CharField(
+#         "Which institution(s) are you affiliated with?", max_length=100, null=True, blank=True
+#     )
+#     purpose = models.CharField(
+#         "What would you like to do?", max_length=100, null=True, blank=True)
+#     start_session = models.CharField(
+#         "When would you like to start?", max_length=100, null=True, blank=True
+#     )
 
-    # IU Student?
+#     # IU Student?
 
-    former_enrollment = models.BooleanField(default=False)
-    currently_enrolled = models.BooleanField(default=False)
+#     former_enrollment = models.BooleanField(default=False)
+#     currently_enrolled = models.BooleanField(default=False)
 
-    # if yes
+#     # if yes
 
-    iu_id = models.CharField(max_length=40)
-    first_name = models.CharField(
-        max_length=40
-    )
-    last_name = models.CharField(
-        max_length=40
-    )
-    email = models.EmailField(
-        max_length=40, unique=True
-    )
-    confirm_email = models.EmailField(
-        max_length=40, unique=True
-    )
-    phone_number = models.CharField(
-        max_length=20, null=True, blank=True
-    )
-    contact_method = models.CharField(
-        max_length=10, null=True, blank=True
-    )
+#     iu_id = models.CharField(max_length=40)
+#     first_name = models.CharField(
+#         max_length=40
+#     )
+#     last_name = models.CharField(
+#         max_length=40
+#     )
+#     email = models.EmailField(
+#         max_length=40, unique=True
+#     )
+#     confirm_email = models.EmailField(
+#         max_length=40, unique=True
+#     )
+#     phone_number = models.CharField(
+#         max_length=20, null=True, blank=True
+#     )
+#     contact_method = models.CharField(
+#         max_length=10, null=True, blank=True
+#     )
 
-    date_of_birth = models.DateField(
-        null=True, blank=True
-    )
-    citizenship = models.CharField(
-        "Which status best describes you?", max_length=40, null=True, blank=True
-    )
+#     date_of_birth = models.DateField(
+#         null=True, blank=True
+#     )
+#     citizenship = models.CharField(
+#         "Which status best describes you?", max_length=40, null=True, blank=True
+#     )
 
-    # Veteran Member?
+#     # Veteran Member?
 
-    veteran_member = models.BooleanField(default=False)
-    veteran_member = models.CharField(
-        max_length=40, null=True, blank=True
-    )
-    benefits = models.BooleanField(default=False)
+#     veteran_member = models.BooleanField(default=False)
+#     veteran_member = models.CharField(
+#         max_length=40, null=True, blank=True
+#     )
+#     benefits = models.BooleanField(default=False)
 
-    # Current Address
+#     # Current Address
 
-    address = models.TextField(
-        blank=True, null=True
-    )
-    city = models.CharField(
-        max_length=40, blank=True, null=True
-    )
-    state = models.CharField(
-        max_length=150, null=True, blank=True
-    )
-    postal_code = models.CharField(
-        max_length=12, null=True, blank=True
-    )
-    country = models.CharField(
-        max_length=40, null=True, blank=True
-    )
-    is_permanent_address = models.BooleanField(
-        default=False
-    )
-    residency_in_indiana = models.CharField(
-        max_length=40, null=True, blank=True
-    )
-    gender = models.CharField(
-        max_length=6, null=True, blank=True
-    )
-    gender_info = models.CharField(
-        max_length=70, null=True, blank=True
-    )
-    # Former Name
+#     address = models.TextField(
+#         blank=True, null=True
+#     )
+#     city = models.CharField(
+#         max_length=40, blank=True, null=True
+#     )
+#     state = models.CharField(
+#         max_length=150, null=True, blank=True
+#     )
+#     postal_code = models.CharField(
+#         max_length=12, null=True, blank=True
+#     )
+#     country = models.CharField(
+#         max_length=40, null=True, blank=True
+#     )
+#     is_permanent_address = models.BooleanField(
+#         default=False
+#     )
+#     residency_in_indiana = models.CharField(
+#         max_length=40, null=True, blank=True
+#     )
+#     gender = models.CharField(
+#         max_length=6, null=True, blank=True
+#     )
+#     gender_info = models.CharField(
+#         max_length=70, null=True, blank=True
+#     )
+#     # Former Name
 
-    former_name = models.CharField(
-        max_length=80, null=True, blank=True
-    )
-    former_name_2 = models.CharField(
-        max_length=80, null=True, blank=True
-    )
-    former_name_3 = models.CharField(
-        max_length=80, null=True, blank=True
-    )
-    former_name_4 = models.CharField(
-        max_length=80, null=True, blank=True
-    )
-    former_name_5 = models.CharField(
-        max_length=80, null=True, blank=True
-    )
+#     former_name = models.CharField(
+#         max_length=80, null=True, blank=True
+#     )
+#     former_name_2 = models.CharField(
+#         max_length=80, null=True, blank=True
+#     )
+#     former_name_3 = models.CharField(
+#         max_length=80, null=True, blank=True
+#     )
+#     former_name_4 = models.CharField(
+#         max_length=80, null=True, blank=True
+#     )
+#     former_name_5 = models.CharField(
+#         max_length=80, null=True, blank=True
+#     )
 
-    class Meta:
-        """docstring for meta"""
-        verbose_name_plural = "Student Details"
+#     class Meta:
+#         """docstring for meta"""
+#         verbose_name_plural = "Student Details"
