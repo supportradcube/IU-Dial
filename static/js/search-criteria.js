7b11060f3752405f11db88bf041eee1553fa9545
smritisingh_mobiloitte <smriti.singh@mobiloitte.in>
2018-12-24 18:36:22 +0530

    Testing Environment Setup & Database Structure : Student Module

--- static/js/search-criteria.js
+++ static/js/search-criteria.js
@@ -2,7 +2,6 @@ function student_tab() {
     document.getElementById("student-search-criteria").style.display = "block";
     document.getElementById("enrollment-search-criteria").style.display = "none";
     document.getElementById("course-search-criteria").style.display = "none";
-    document.getElementById("export-search-results").style.display = "block";
     document.getElementById("footer").setAttribute('style', 'margin-top:40px');
 
 }
@@ -11,7 +10,6 @@ function course_tab() {
     document.getElementById("student-search-criteria").style.display = "none";
     document.getElementById("enrollment-search-criteria").style.display = "none";
     document.getElementById("course-search-criteria").style.display = "block";
-    document.getElementById("export-search-results").style.display = "block";
     document.getElementById("footer").setAttribute('style', 'margin-top:40px');
 
 }
@@ -20,7 +18,6 @@ function enrollment_tab() {
     document.getElementById("enrollment-search-criteria").style.display = "block";
     document.getElementById("student-search-criteria").style.display = "none";
     document.getElementById("course-search-criteria").style.display = "none";
-    document.getElementById("export-search-results").style.display = "block";
     document.getElementById("footer").setAttribute('style', 'margin-top:40px');
 
 }
@@ -30,6 +27,5 @@ function batchexport_tab() {
     document.getElementById("student-search-criteria").style.display = "none";
     document.getElementById("course-search-criteria").style.display = "none";
     document.getElementById("master-container").setAttribute('style', 'min-height:0');
-    document.getElementById("export-search-results").style.display = "none";
     document.getElementById("footer").setAttribute('style', 'margin-top:213px');
 }
